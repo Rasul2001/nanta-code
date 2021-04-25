7353b2d0108d8887c4e625880645218f25403c18
Lenovo <ajdyndzobalaevk@gmail.com>
2021-04-25 15:43:21 +0600

    new code

--- go.mod
+++ go.mod
@@ -1,10 +1,14 @@
-module nanta-code
+module проекты/nanta/go-echo-api
 
-go 1.15
+go 1.12
 
 require (
 	github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0 // indirect
 	github.com/go-martini/martini v0.0.0-20170121215854-22fa46961aab
 	github.com/go-sql-driver/mysql v1.6.0
 	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
 )
--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
-import{
-  "fmt"
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo"
+)
+
+func main() {
+	fmt.Println("Go Program")
+	server := echo.New()
+	server.GET(path.Join("/"), Version)
+
+	godotenv.Load()
+	port := os.Getenv("PORT")
+
+	address := fmt.Sprintf("%s:%s", "0.0.0.0", port)
+	fmt.Println(address)
+	server.Start(address)
+
 }
-func main(){
-  fmt.Println("Go program")
+func Version(context echo.Context) error {
+	return context.JSON(http.StatusOK, map[string]interface{}{"version": 2})
 }
